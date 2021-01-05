439aca4f60a2252204f78e6c42c3b68e3e484f27
vishuchhabra <vishuchhabra1016@gmail.com>
2021-01-05 12:23:35 +0530

    adding files

--- projfrontend/src/Components/Footer/Footer.js
+++ projfrontend/src/Components/Footer/Footer.js
@@ -4,9 +4,12 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-
       {/* <span className="copy"><strong> &copy; Copyright 2020 </strong></span> */}
-      <a href="mailto:vishuchhabra1016@gmail.com"><button className="button"><strong>CONTACT US </strong> </button> </a>
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLSd3D4sAKQbXFvfurhrZob79nnQddDUqaUfeRbszIdKpCi3trw/viewform">
+        <button className="button">
+          <strong>CONTACT US</strong>
+        </button>
+      </a>
     </footer>
   );
 }
--- projfrontend/src/Components/Header/Header.js
+++ projfrontend/src/Components/Header/Header.js
@@ -4,7 +4,10 @@ import "./Header.css";
 function Header() {
   return (
     <div className="site__navbar">
-      <h1 className="site__heading">HelpMe <span ><i style={{"fontSize":"20px"}}>-serving for soceity</i></span></h1>
+      <h1 className="site__heading">HelpMe</h1>
+      <span>
+        <i style={{ fontSize: "20px" }}>Serving For Society</i>
+      </span>
       <p className="site__subtitle"></p>
     </div>
   );
