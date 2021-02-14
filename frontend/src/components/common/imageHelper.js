a686e3e09b7545dc30ff25c54507de3063630a59
vishuchhabra <vishuchhabra1016@gmail.com>
2021-02-14 23:02:09 +0530

    adding files

--- frontend/src/components/common/imageHelper.js
+++ frontend/src/components/common/imageHelper.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../../styles/app.css'
 import {API} from '../../backend'
 
 // Fetching the Image
 const ImageHelper = ({product}) =>{
-    const imageurl = product ?  `http://localhost:4001/api/product/photo/${product._id}`: `https://images.pexels.com/photos/3561339/pexels-photo-3561339.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`
-    
+
+    const imageurl = ( product ?  `${API}/product/photo/${product._id}`: `https://images.pexels.com/photos/3561339/pexels-photo-3561339.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`)
     return (
         <div >
             {imageurl &&
